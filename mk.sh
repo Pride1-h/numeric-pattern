6ee21fd1082b6ff222ed71dfada05916107435ba
Pride1-h <57586602+Pride1-h@users.noreply.github.com>
2020-01-14 11:07:54 -0500

    Create mk.sh

---
+++ mk.sh
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+ int j,i;
+ for(i=1;i<=10;i++)
+ {
+ for(j=1;j<=i;j++)
+ {
+ printf("\t%d"j);
+ }
+ printf("\n");
+ }
+ getch();
+ return 0;
+ }
